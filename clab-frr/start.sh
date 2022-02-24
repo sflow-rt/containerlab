cf4c6e65667a89a7233086e979ece296f32bb754
pphaal <peter.phaal@gmail.com>
2022-02-24 14:43:29 -0800

    Add EVPN support

--- clab-frr/start.sh
+++ clab-frr/start.sh
@@ -6,6 +6,7 @@ SAMPLING="${SAMPLING:-1000}"
 NEIGHBORS="${NEIGHBORS:-eth1 eth2}"
 HOSTPORT="${HOSTPORT:-eth3}"
 HOSTNET="${HOSTNET:-none}"
+EVPNSRC="${EVPNSRC:-none}"
 
 CONF='/etc/hsflowd.conf'
 
@@ -23,6 +24,7 @@ fi
 printf "}\n" >> $CONF
 
 BGP=/etc/frr/bgpd.conf
+ZEBRA=/etc/frr/zebra.conf
 LOCAL_ADDR=`hostname -i`
 LOCAL_AS="${LOCAL_AS:-65000}"
 sed -i "s/LOCAL_AS/$LOCAL_AS/g" $BGP
@@ -32,31 +34,58 @@ do
 done
 printf " !\n" >> $BGP
 
-if [ "$HOSTNET" != "none" ]; then
-  printf " address-family ipv4 unicast\n" >> $BGP
-  printf "  redistribute connected route-map HOST_ROUTES\n" >> $BGP
+if [ "$HOSTNET" == "evpn" ]; then 
+  printf " address-family l2vpn evpn\n" >> $BGP
+  printf "  neighbor fabric activate\n" >> $BGP
+  if [ "$EVPNSRC" != "none" ]; then
+    printf "  advertise-all-vni\n" >> $BGP
+  fi
   printf " exit-address-family\n" >> $BGP
-  printf "!\n" >> $BGP
-  printf "exit\n" >> $BGP
-  printf "!\n" >> $BGP
-  printf "route-map HOST_ROUTES permit 10\n" >> $BGP
-  printf "  match interface $HOSTPORT\n" >> $BGP
-  printf "exit\n" >> $BGP
-  printf "!\n" >> $BGP
+  printf " !\n" >> $BGP
+  if [ "$EVPNSRC" != "none" ]; then
+    printf " address-family ipv4 unicast\n" >> $BGP
+    printf "  network $EVPNSRC/32\n" >> $BGP
+    printf " exit-address-family\n" >> $BGP
+    printf "!\n" >> $BGP
+    printf "exit\n" >> $BGP
+    printf "!\n" >> $BGP
+  fi
+else
+  if [ "$HOSTNET" != "none" ]; then
+    printf " address-family ipv4 unicast\n" >> $BGP
+    printf "  redistribute connected route-map HOST_ROUTES\n" >> $BGP
+    printf " exit-address-family\n" >> $BGP
+    printf "!\n" >> $BGP
+    printf "exit\n" >> $BGP
+    printf "!\n" >> $BGP
+    printf "route-map HOST_ROUTES permit 10\n" >> $BGP
+    printf "  match interface $HOSTPORT\n" >> $BGP
+    printf "exit\n" >> $BGP
+    printf "!\n" >> $BGP
 
-  ZEBRA=/etc/frr/zebra.conf
-  printf "interface $HOSTPORT\n" >> $ZEBRA
-  printf " ip address $HOSTNET\n" >> $ZEBRA
-  printf "exit\n" >> $ZEBRA
-  printf "!\n" >> $ZEBRA
+    printf "interface $HOSTPORT\n" >> $ZEBRA
+    printf " ip address $HOSTNET\n" >> $ZEBRA
+    printf "exit\n" >> $ZEBRA
+    printf "!\n" >> $ZEBRA
+  fi
 fi
-printf "end\n" >> $BGP
 
 chown -R frr:frr /etc/frr
 
 sysctl -w net.ipv4.fib_multipath_hash_policy=1
 
 while [ ! -f /tmp/initialized ]; do sleep 1; done
+
+if [ "$HOSTNET" == "evpn" ] && [ "$EVPNSRC" != "none" ]; then
+  ip addr add $EVPNSRC/32 dev lo
+  ip link add vxlan10 type vxlan id 10 dstport 0 local $EVPNSRC
+  brctl addbr br10
+  brctl addif br10 vxlan10
+  brctl stp br10 off
+  ip link set up dev br10
+  ip link set up dev vxlan10
+  brctl addif br10 $HOSTPORT
+fi
 
 /usr/sbin/hsflowd
 exec /usr/lib/frr/docker-start
