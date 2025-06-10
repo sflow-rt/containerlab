2e49d4db5e4c21245d7878c1198b6bcc5b32b890
pphaal <peter.phaal@gmail.com>
2025-06-10 09:27:14 -0700

    update to latest frr and clab releases

--- clab-frr/start.sh
+++ clab-frr/start.sh
@@ -29,99 +29,97 @@ if [ "$HOSTNET" != "none" ]; then
 fi
 printf "}\n" >> $CONF
 
-BGP=/etc/frr/bgpd.conf
-ZEBRA=/etc/frr/zebra.conf
-STATICD=/etc/frr/staticd.conf
+BGP=/etc/frr/frr.conf
 LOCAL_ADDR=`hostname -i`
 LOCAL_AS="${LOCAL_AS:-65000}"
 sed -i "s/LOCAL_AS/$LOCAL_AS/g" $BGP
 if [ "$RTBH" != "none" ]; then
-  printf " neighbor fabric ebgp-multihop 255\n" >> $BGP
+  printf "neighbor fabric ebgp-multihop 255\n" >> $BGP
 fi
 for dev in ${NEIGHBORS}
 do
-  printf " neighbor $dev interface peer-group fabric\n" >> $BGP
+  printf "neighbor $dev interface peer-group fabric\n" >> $BGP
 done
 if [ "$CTLASN" != "none" ]; then
-  printf " neighbor $COLLECTOR remote-as $CTLASN\n" >> $BGP
-  printf " neighbor $COLLECTOR port 1179\n" >> $BGP
-  printf " neighbor $COLLECTOR timers connect 10\n" >> $BGP
-  printf " !\n" >> $BGP
+  printf "neighbor $COLLECTOR remote-as $CTLASN\n" >> $BGP
+  printf "neighbor $COLLECTOR port 1179\n" >> $BGP
+  printf "neighbor $COLLECTOR timers connect 10\n" >> $BGP
+  printf "\n" >> $BGP
 fi
 if [ "$FLOWSPEC" == "yes" ]; then
-  printf " address-family ipv4 flowspec\n" >> $BGP
-  printf "  neighbor fabric activate\n" >> $BGP
+  printf "address-family ipv4 flowspec\n" >> $BGP
+  printf "neighbor fabric activate\n" >> $BGP
   if [ "$CTLASN" != "none" ]; then 
-    printf "  neighbor $COLLECTOR activate\n" >> $BGP
+    printf "neighbor $COLLECTOR activate\n" >> $BGP
   fi
-  printf " exit-address-family\n" >> $BGP
+  printf "exit-address-family\n" >> $BGP
 fi
-printf " !\n" >> $BGP
+printf "\n" >> $BGP
 if [ "$HOSTNET" == "evpn" ]; then 
-  printf " address-family l2vpn evpn\n" >> $BGP
-  printf "  neighbor fabric activate\n" >> $BGP
+  printf "address-family l2vpn evpn\n" >> $BGP
+  printf "neighbor fabric activate\n" >> $BGP
   if [ "$EVPNSRC" != "none" ]; then
-    printf "  advertise-all-vni\n" >> $BGP
+    printf "advertise-all-vni\n" >> $BGP
   fi
-  printf " exit-address-family\n" >> $BGP
-  printf " !\n" >> $BGP
+  printf "exit-address-family\n" >> $BGP
+  printf "\n" >> $BGP
   if [ "$EVPNSRC" != "none" ]; then
-    printf " address-family ipv4 unicast\n" >> $BGP
-    printf "  network $EVPNSRC/32\n" >> $BGP
-    printf " exit-address-family\n" >> $BGP
-    printf "!\n" >> $BGP
+    printf "address-family ipv4 unicast\n" >> $BGP
+    printf "network $EVPNSRC/32\n" >> $BGP
+    printf "exit-address-family\n" >> $BGP
+    printf "\n" >> $BGP
     printf "exit\n" >> $BGP
-    printf "!\n" >> $BGP
+    printf "\n" >> $BGP
   fi
 else
   if [ "$HOSTNET" != "none" ]; then
-    printf " address-family ipv4 unicast\n" >> $BGP
-    printf "  redistribute connected route-map HOST_ROUTES\n" >> $BGP
+    printf "address-family ipv4 unicast\n" >> $BGP
+    printf "redistribute connected route-map HOST_ROUTES\n" >> $BGP
     if [ "$RTBH" != "none" ]; then
-      printf "  neighbor fabric route-map RTBH in\n" >> $BGP
+      printf "neighbor fabric route-map RTBH in\n" >> $BGP
     fi
-    printf " exit-address-family\n" >> $BGP
-    printf "!\n" >> $BGP
-    printf " address-family ipv6 unicast\n" >> $BGP
-    printf "  redistribute connected route-map HOST_ROUTES\n" >> $BGP
-    printf "  neighbor fabric activate\n" >> $BGP
-    printf " exit-address-family\n" >> $BGP
-    printf "!\n" >> $BGP
+    printf "exit-address-family\n" >> $BGP
+    printf "\n" >> $BGP
+    printf "address-family ipv6 unicast\n" >> $BGP
+    printf "redistribute connected route-map HOST_ROUTES\n" >> $BGP
+    printf "neighbor fabric activate\n" >> $BGP
+    printf "exit-address-family\n" >> $BGP
+    printf "\n" >> $BGP
     printf "exit\n" >> $BGP
-    printf "!\n" >> $BGP
+    printf "\n" >> $BGP
     printf "route-map HOST_ROUTES permit 10\n" >> $BGP
-    printf "  match interface $HOSTPORT\n" >> $BGP
+    printf "match interface $HOSTPORT\n" >> $BGP
     printf "exit\n" >> $BGP
-    printf "!\n" >> $BGP
-    printf "interface $HOSTPORT\n" >> $ZEBRA
-    printf " ip address $HOSTNET\n" >> $ZEBRA
+    printf "\n" >> $BGP
+    printf "interface $HOSTPORT\n" >> $BGP
+    printf "ip address $HOSTNET\n" >> $BGP
     if [ "HOSTNET6" != "none" ]; then
-      printf " ipv6 address $HOSTNET6\n" >> $ZEBRA
+      printf "ipv6 address $HOSTNET6\n" >> $BGP
     fi
-    printf "exit\n" >> $ZEBRA
-    printf "!\n" >> $ZEBRA
-  else
-    printf " address-family ipv6 unicast\n" >> $BGP
-    printf "   neighbor fabric activate\n" >> $BGP
-    printf " exit-address-family\n" >> $BGP
-    printf "!\n" >> $BGP
     printf "exit\n" >> $BGP
-    printf "!\n" >> $BGP
+    printf "\n" >> $BGP
+  else
+    printf "address-family ipv6 unicast\n" >> $BGP
+    printf "neighbor fabric activate\n" >> $BGP
+    printf "exit-address-family\n" >> $BGP
+    printf "\n" >> $BGP
+    printf "exit\n" >> $BGP
+    printf "\n" >> $BGP
   fi
 fi
 if [ "$RTBH" != "none" ]; then
   printf "bgp community-list standard BLACKHOLE seq 5 permit blackhole\n" >> $BGP
-  printf "!\n" >> $BGP
+  printf "\n" >> $BGP
   printf "route-map RTBH permit 10\n" >> $BGP
-  printf "  match community BLACKHOLE\n" >> $BGP
-  printf "  set ip next-hop $RTBH\n" >> $BGP
+  printf "match community BLACKHOLE\n" >> $BGP
+  printf "set ip next-hop $RTBH\n" >> $BGP
   printf "exit\n" >> $BGP
-  printf "!\n" >> $BGP
+  printf "\n" >> $BGP
   printf "route-map RTBH permit 20\n" >> $BGP
   printf "exit\n" >> $BGP
-  printf "!\n" >> $BGP
-  printf "ip route $RTBH/32 Null0\n" >> $STATICD
-  printf "!\n" >> $STATICD
+  printf "\n" >> $BGP
+  printf "ip route $RTBH/32 Null0\n" >> $BGP
+  printf "\n" >> $BGP
 fi
 
 chown -R frr:frr /etc/frr
